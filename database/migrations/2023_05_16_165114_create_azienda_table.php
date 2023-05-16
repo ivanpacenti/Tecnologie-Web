2c3c5714b7737c8449cd36b667539ee90f8177d9
ivanpacenti <s1103326@studenti.univpm.it>
2023-05-16 21:26:42 +0200

    prototipo tabella database, con seeders

--- database/migrations/2023_05_16_165114_create_azienda_table.php
+++ database/migrations/2023_05_16_165114_create_azienda_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('azienda', function (Blueprint $table) {
             $table->string('nome');
+            $table->integer('dipendenti');
+            $table->increments('id');
+
         });
     }
 
