6f39b09c44c08be1e3b4f8c3b380f20948d4baa4
ivanpacenti <s1103326@studenti.univpm.it>
2023-05-18 18:24:16 +0200

    funzione js migliorata

--- public/js/filtroAziende.js
+++ public/js/filtroAziende.js
@@ -1,22 +1,40 @@
 // Aggiungi un evento di ascolto ai checkbox
 var checkboxes = document.querySelectorAll('.check');
+
 checkboxes.forEach(function(checkbox) {
     checkbox.addEventListener('change', handleCheckboxChange);
+
 });
+
+
+
 
 // Funzione per gestire il cambio di stato dei checkbox
 function handleCheckboxChange() {
     var checkboxId = this.id;
+    var isChecked = this.checked;
 
     // Seleziona tutti i div filtrabili
     var divsFiltrabili = document.querySelectorAll('.containerCoupon');
 
+    // Verifica se almeno un checkbox è selezionato
+    var isAnyCheckboxChecked = Array.from(checkboxes).some(function(checkbox) {
+        return checkbox.checked;
+    });
+
+    // Se nessun checkbox è selezionato, mostra tutti i div
+    if (!isAnyCheckboxChecked) {
+        divsFiltrabili.forEach(function(div) {
+            div.style.display = 'block';
+        });
+        return;
+    }
+
     // Itera sui div filtrabili e controlla se devono essere mostrati o nascosti
     divsFiltrabili.forEach(function(div) {
-        // Controlla se il checkbox corrispondente è selezionato
-        var checkbox = document.getElementById(checkboxId);
         var divId = div.id;
-        if (checkbox.checked && checkboxId === divId) {
+
+        if (isChecked && checkboxId === divId) {
             // Mostra il div se il checkbox è selezionato e il suo id corrisponde al divId
             div.style.display = 'block';
         } else {
@@ -24,4 +42,5 @@ function handleCheckboxChange() {
             div.style.display = 'none';
         }
     });
+
 }
