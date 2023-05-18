391f7790aa6d4887dbcc07531277762b041d481a
Valeria Cannone <93262353+valeriac23@users.noreply.github.com>
2023-05-18 23:08:58 +0200

    elenco dinamico faq e inserimento bottone (da rivedere)

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -27,8 +27,42 @@
                     <div class="container-faqs">
                         <h2 class="h2"> FAQ'S </h2>
                         <p> Visualizza qui le domande più frequenti:</p>
-                    </div>
+                        <ul class="faq-list1">
+                            <il>1.</il>
+                                <div>
+                                    domanda 1
+                                </div>
+                            <il>2.</il>
+                                <div>
+                                    DOMANDA 2
+                                </div>
+                            <il>3.</il>
+                                <div>
+                                    domanda treee
+                                </div>
+                        </ul>
+
+                        <!-- Aggiunge elementi alla lista -->
+                        <script>
+                        var counter = n;
+                        function addListItem() {
+                        var ul = document.getElementById('faq-list1');
+                        var li = document.createElement('li');
+                        li.appendChild(document.createTextNode('Voce ' + counter));
+                        ul.appendChild(li);
+                        counter++;
+                        }
+                        //bottone per visualizzare altre faqs
+                        <button id="addOtherF">Visualizza altro</button>
+
+                        // Aggiungi un gestore di eventi al pulsante "Visualizza altro"
+                        var addButton = document.getElementById('addOtherF');
+                        addButton.addEventListener('click', addListItem);
+                        </script>
+                       </div>
                 </div>
             </div>
     </footer>
 </html>
+
+<
