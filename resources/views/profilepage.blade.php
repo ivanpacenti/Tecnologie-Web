d9263ca5e19b3daef874b5fc667fe3b2e4ba90e7
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-05-18 12:13:20 +0200

    update profilo (quasi finito) + commenti

--- resources/views/profilepage.blade.php
+++ resources/views/profilepage.blade.php
@@ -4,9 +4,16 @@
 
 @section('content')
 <link rel="stylesheet" type="text/css" href="{{ asset('css/profile_design.css') }}" >
+<!-- questo container contiene tutti i campi presenti nel pannello ed è di tipo felx in modo tale da organizzare meglio le cose
+     * 'containerfield' è un altro container che contiene le informazioni che vengono estratti dal database
+     * 'imagecontainer' contiene l'immagine di default per il profilo utente
+     * 'edit' è un button che collega ad una pannlelo per la modifica dei dati personali (da implementare)-->
 <div class="container1">
     <div class="titolo">
         Dettagli utente
+    </div>
+    <div class="imagecontainer">
+        <img src="img/shop.jpg" alt="immagine coupon" style="border-radius: 50%;" width=150px height=150px>
     </div>
     <div class="containerfield">
         nome
@@ -29,6 +36,8 @@
     <div class="containerfield">
         nome
     </div>
+
+    <button class="edit">Modifica dati personali</div>
 </div>
 
 @endsection('content')
