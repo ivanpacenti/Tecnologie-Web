846b4f8d7bb59d4298670d71da4a218c59f3dd2b
Nicola Picciafuoco <s1099360@studenti.univpm.it>
2023-05-19 16:20:10 +0200

    creazione migrazione users

--- database/migrations/2023_05_17_171302_create_utentes_table.php
+++ database/migrations/2023_05_17_171302_create_utentes_table.php
@@ -37,3 +37,23 @@ return new class extends Migration
         Schema::dropIfExists('utentes');
     }
 };
+
+
+
+// breze gia mette questa e noi dobbiamo introdurre nome e cognome, il campo username e il campo role, definito come una stringa di 10 caratteri
+// può essere admin o user, per il campo ruolo definiamo ruolo di defaul che è user, in sintesi definisco, questo crea la tabella andiamo a vedere il model
+//public function up()
+//{
+//    Schema::create('users', function (Blueprint $table) {
+//        $table->bigIncrements('id');
+//        $table->string('name');
+//        $table->string('surname');
+//        $table->string('email');
+//        $table->timestamp('email_verified_at')->nullable();
+//        $table->string('username',20);
+//        $table->string('password');
+//        $table->string('role',10)->default('user');
+//        $table->rememberToken();
+//        $table->timestamps();
+//    });
+//}
