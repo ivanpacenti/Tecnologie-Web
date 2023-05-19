570876cbd06aafa1d86789ab4ac57c06389c6c3b
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-05-19 16:34:53 +0200

    styling della pagina di modifica delle faq finita

--- resources/views/faqsedit.blade.php
+++ resources/views/faqsedit.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
     <link rel="stylesheet" type="text/css" href="{{ asset('css/admin_design.css') }}" >
-<div class="maincontainer">
+{{--<div class="maincontainer">
     <form class="form" method="post" action="{{ route('edit')}}" >
         @csrf
         <div class="form-box">
@@ -19,5 +19,24 @@
         </div>
             <button type="submit"> AGGIORNA </button>
     </form>
-</div>
+</div>--}}
+    {!! Form::model($faq, ['route' => ['edit', $faq['id']], 'class' => 'form']) !!}
+    {!! Form::token() !!}
+        <div class="form-group">
+            {!! Form::label('id', 'ID') !!}
+            {!! Form::text('id', $faq['id'], ['class' => 'form-input', 'placeholder' => 'Inserisci ID']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('domanda', 'Domanda') !!}
+            {!! Form::text('domanda', $faq['domanda'], ['class' => 'form-input', 'placeholder' => 'Inserisci domanda']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('risposta', 'Risposta') !!}
+            {!! Form::text('risposta', $faq['risposta'], ['class' => 'form-input','placeholder' => 'Inserisci risposta']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Aggiorna', ['class' => 'formbutton']) !!}
+        </div>
+    {!! Form::close() !!}
+
 @endsection('content')
