714b32d1ef1de1832bb6591b6dbd0023b8b84fde
ivanpacenti <s1103326@studenti.univpm.it>
2023-05-20 17:50:58 +0200

    prototipo di filtro delle offerte a livello di query su db

--- public/js/rigeneraVista.js
+++ public/js/rigeneraVista.js
@@ -1,26 +1,34 @@
 var checkboxes = document.querySelectorAll('.check');
+var checkboxIds = [];
+
+// Memorizza gli ID dei checkbox selezionati
+for (var i = 0; i < checkboxes.length; i++) {
+    var checkboxId = checkboxes[i].value;
+
+    checkboxIds.push(checkboxId);
+
+    // Aggiungi un evento di ascolto ai checkbox
+    checkboxes[i].addEventListener('change', function() {
+        var checkboxId = this.value;
+        var checkboxState = this.checked;
+        if (checkboxState) {
+            document.getElementById('formCheckbox').submit(); // Esempio di invio del modulo
+            // Salva lo stato selezionato nel localStorage
+            localStorage.setItem('checkbox_' + checkboxId, checkboxState);
+        }
+    });
+}
 
 // Ripristina lo stato dei checkbox al caricamento della pagina
 window.addEventListener('DOMContentLoaded', function() {
-    checkboxes.forEach(function(checkbox) {
-        var checkboxId = checkbox.dataset.id;
+    for (var i = 0; i < checkboxIds.length; i++) {
+        var checkboxId = checkboxIds[i];
         var checkboxState = localStorage.getItem('checkbox_' + checkboxId);
 
+        var checkbox = document.querySelector('[data-id="' + checkboxId + '"]');
         if (checkboxState === 'true') {
             checkbox.checked = true;
         }
-    });
+    }
 });
 
-// Aggiungi un evento di ascolto ai checkbox
-checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
-        var checkboxId = this.dataset.id;
-        var checkboxState = this.checked;
-
-        // Salva lo stato selezionato nel localStorage
-        localStorage.setItem('checkbox_' + checkboxId, checkboxState);
-
-        document.getElementById('formCheckbox').submit(); // Esempio di invio del modulo
-    });
-});
