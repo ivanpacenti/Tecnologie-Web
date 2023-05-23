429b010e0a1c469985b2315b4096b1390ed5994d
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-05-23 19:43:25 +0200

    aggiornamento pagina di stampa ed informazione del coupon

--- resources/views/userView/coupongiaComprato.blade.php
+++ resources/views/userView/coupongiaComprato.blade.php
@@ -3,10 +3,11 @@
 @section('title','Home')
 
 @section('content')
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/home_pub_design.css') }}" >
-    <div class="product-details">
-        <h1> Benvenuto {{ Auth::user()->name }}  {{ Auth::user()->surname }} </h1>
-        <h3> Ci dispiace informarla che ha già acquistato questa offerta</h3>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/catalogo_pub_design.css') }}" >
+    <div class="coupon-informations">
+        <h1> Si è verificato un errore... </h1>
+        <h3> {{ Auth::user()->name }}  {{ Auth::user()->surname }} ci dispiace informarla che ha già acquistato questa offerta</h3>
         <h3> Siamo sicuri ne troverà altre di suo gradimento, continui pure su catalogo</h3>
+        <button onclick="location.href='{{ route('catalogo') }}'" class="form-button">Torna al catalogo</button>
     </div>
 @endsection
