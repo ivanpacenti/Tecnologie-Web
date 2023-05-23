7c0c5499cba9bb06b2f0d09a2f4bec74743d9659
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-05-23 19:12:29 +0200

    aggiornamento pagina di dettaglio del prodotto

--- resources/views/offerdetail.blade.php
+++ resources/views/offerdetail.blade.php
@@ -4,47 +4,44 @@
 
 @section('content')
 
-<link rel="stylesheet" type="text/css" href="{{ asset('css/home_pub_design.css') }}" >
+<link rel="stylesheet" type="text/css" href="{{ asset('css/catalogo_pub_design.css') }}" >
 <body>
     <div class="product-details">
         <div class="leftcontainer">
             <div class="product-detailsimg">
-                <img src="{{ $offerta->immagine }}"  style="border-radius: 15px">
+                <img src="{{asset('img/lacoste_logo.jpeg')}}{{--{{ $offerta->immagine }}--}}" class="img_detail">
             </div>
         </div>
         <div class="rightcontainer">
+            <div class="id">
+                <p> ID: {{ $offerta->id }}</p>
+            </div>
             <div class="descrizione">
-                <h3> DESCRIZIONE: {{ $offerta->descrizione }}</h3>
+                <p> DESCRIZIONE: {{ $offerta->descrizione }}</p>
             </div>
 
             <div class="modalita">
-                <h3> MODALITA':   {{ $offerta->modalità }}</h3>
+                <p> MODALITA': {{ $offerta->modalità }}</p>
             </div>
-
-{{--            <div class="id">--}}
-{{--                {{ $offerta->id }}--}}
-{{--            </div>--}}
 
             <div class="luogofruizione">
-                <h3> LUOGO DI FRUIZIONE:  {{ $offerta->luogoFruizione }}</h3>
+                <p> LUOGO DI FRUIZIONE: {{ $offerta->luogoFruizione }}</p>
             </div>
+            <div class="scadenza">
+                <div class="dataInizio">
+                    <p> DATA INIZIO: {{ $offerta->dataInizio }}</p>
+                </div>
 
-            <div class="dataInizio">
-                <h3> DATA INIZIO:  {{ $offerta->dataInizio }}</h3>
+                <div class="dataFine">
+                    <p> DATA FINE: {{ $offerta->dataFine }}</p>
+                </div>
             </div>
-
-            <div class="dataFine">
-
-                <h3> DATA FINE:{{ $offerta->dataFine }}</h3>
-
-            </div>
+            @can('isUser')
+                <button onclick="location.href='{{ route('stampa', ['coupon_id' => $offerta->id, 'user_id' => Auth::id()]) }}'" class="form-button">Acquista</button>
+            @endcan
         </div>
     </div>
 
 </body>
-
-@can('isUser')
-    <button onclick="location.href='{{ route('stampa', ['coupon_id' => $offerta->id, 'user_id' => Auth::id()]) }}'">COMPRA</button>
-@endcan
 
 @endsection('content')
--- resources/views/userView/coupongiaComprato.blade.php
+++ resources/views/userView/coupongiaComprato.blade.php
@@ -3,9 +3,10 @@
 @section('title','Home')
 
 @section('content')
-    <h1> benvenuto    {{ Auth::user()->name }}  {{ Auth::user()->surname }} </h1>
-    <h3> Ci dispiace informarla che ha già acquistato questa offerta</h3>
-
-    <h3> Siamo sicuri ne troverà altre di suo gradimento, continui pure su catalogo</h3>
-
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/home_pub_design.css') }}" >
+    <div class="product-details">
+        <h1> Benvenuto {{ Auth::user()->name }}  {{ Auth::user()->surname }} </h1>
+        <h3> Ci dispiace informarla che ha già acquistato questa offerta</h3>
+        <h3> Siamo sicuri ne troverà altre di suo gradimento, continui pure su catalogo</h3>
+    </div>
 @endsection
