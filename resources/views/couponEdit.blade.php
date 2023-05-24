3eb2875afbedb99161c3757732631ca18866ca0a
Valeria Cannone <93262353+valeriac23@users.noreply.github.com>
2023-05-24 21:43:20 +0200

    Modifica pagina generica Coupon (da completare)

--- resources/views/couponEdit.blade.php
+++ resources/views/couponEdit.blade.php
@@ -4,72 +4,45 @@
 
 @section('content')
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/admin_design.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/admin_desing.css') }}" >
 
     <div class="maincontainer">
-        <h1> CRUD PROMOZIONI</h1>
-<button class="buttonbar"> Aggiungi Promozione</button>
-    </div>
-    <div class="main-box-az">
-        <h2>Modifica Promozioni</h2>
-    </div>
-    <div class="container1">
-@isset($offerte)
-    <form action="{{ route('filtroOfferte') }}" method="POST" id="formCheckbox">
-        <!--  csfr passa il token della richiesta al controller-->
-        @csrf
-         <div class="checkboxprincipal">
-             <!-- aggiungere filtro? -->
-           <!-- <div class="checktitle">Filtra per azienda</div>
-            @foreach($aziende as $azienda)
-                <label class="containercheck">{{$azienda->nome}}
-                    <input class ="check" type="checkbox" value="{{ $azienda->id }}" id="{{ $azienda->id }}" name="aziende_selezionate[]" onclick="checkClick()">
-                    <span class="checkmark">
-                    </span>
-                </label>
-            @endforeach-->
-            @endisset
-        </div>
-    </form>
-    </div>
-    <div class="container">
+        <h1> PROMOZIONI</h1>
+        <button class="buttonbar2">Aggiungi Promozioni</button>
+
         @isset($offerte)
-           <div class="container2">
                 @foreach($offerte as $offerta)
-                        <div class="imgslot">{{$aziende->find($offerta->emissione->azienda)->nome}}
-                            <div class="main-box-az" id="{{$aziende->find($offerta->emissione->azienda)->nome}}">
-                            <img src="{{ $offerta->immagine }}" alt="Imagine1" style="border-radius: 20px" width=10% height=10%>
-                        </div>
-                        <div class="textslot">
-                            <div class="description">
-                                <div class="testocard">
-                                    <p>
-                                        <a href="{{ route('offerdetail', ['id' => $offerta->id]) }}">{{$offerta->descrizione}}</a>
-                                        <a href="{{route('offerdetail',['id' => $offerta->id] )}}">{{$offerta->modalità}}</a>
-                                        <a href="{{route('offerdetail',['id' => $offerta->id] )}}">{{$offerta->luogoFruizione}}</a>
-                                        <a href="{{route('offerdetail',['id' => $offerta->id] )}}">{{$offerta->dataInizio}}</a>
-                                        <a href="{{route('offerdetail',['id' => $offerta->id] )}}">{{$offerta->dataFine}}</a>
-                                    </p>
+                    <!-- <div class="imgslot">{{$aziende->find($offerta->emissione->azienda)->nome}} -->
+                        <div class="main-box-az" id="{{$aziende->find($offerta->emissione->azienda)->nome}}">
+                            <img src="{{ $offerta->immagine }}" alt="Imagine1" style="border-radius: 20px" width=40% height=40%>
+                                <div class="description">
+                                    <div class="testocard">
+                                        <p>
+                                            <a href="{{ route('offerdetail', ['id' => $offerta->id]) }}">Descrizione: {{$offerta->descrizione}}</a> <br>
+                                            <a href="{{route('offerdetail',['id' => $offerta->id] )}}">Modalità: {{$offerta->modalità}}</a> <br>
+                                            <a href="{{route('offerdetail',['id' => $offerta->id] )}}">Luogo di fruizione: {{$offerta->luogoFruizione}}</a> <br>
+                                            <a href="{{route('offerdetail',['id' => $offerta->id] )}}"> Data Inizio: {{$offerta->dataInizio}}</a> <br>
+                                            <a href="{{route('offerdetail',['id' => $offerta->id] )}}">Data Fine: {{$offerta->dataFine}}</a>
+                                        </p>
+                                        <div class="buttonslot">
+                                            <a href="{{"modify".$offerta['id']}}" class="buttonbar">Modifica </a>
+                                            <button class="buttonbar2">Elimina Promozione</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+
+                        @endforeach
+                        @else
+                        @endisset
+    </div>
                         <div class="buttonslot">
-                        <button class="buttonbar2">Modifica Promozione</button>
-                            <button class="buttonbar2">Elimina Promozione</button>
+                            <button class="buttonbar2">Crea Promozione</button>
+                        </div>
+
+
                     </div>
-
-                @endforeach
-                @else
-@endisset
-<div class="buttonslot">
-     <button class="buttonbar2">Crea Promozione</div>
-
-           </div>
 @endsection
-
-
-
-
 
 
 
