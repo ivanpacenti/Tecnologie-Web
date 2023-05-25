8d408fc523743061faba5b3f2b5fcab287181219
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-05-25 18:48:21 +0200

    aggiunta loghi e completamento aziende nel seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -147,22 +147,23 @@ class DatabaseSeeder extends Seeder {
         );
 
         DB::table('aziendas')->insert([
-            ['partitaIva'=>'7593674937','nome'=>'Lacoste','posizione'=>'Piazza della Repubblica, 14, Ancona AN','descrizione'=>'Lacoste è un iconica marca di moda francese, nota per il suo stile sportivo ed elegante, caratterizzato dal coccodrillo verde ricamato, offrendo abbigliamento di alta qualità per uomini e donne'
+            ['partitaIva'=>'7593674937','nome'=>'Lacoste','posizione'=>'Piazza della Repubblica, 14, Ancona AN','descrizione'=>'Lacoste è un iconica marca di moda francese, nota per il suo stile sportivo ed elegante, caratterizzato dal coccodrillo verde ricamato, offrendo abbigliamento di alta qualità per uomini e donne.'
                 ,'tipologia'=>'ONLINE','logo'=>'img/lacoste_logo.jpeg'],
             ['partitaIva'=>'11111111111','nome'=>'Louis Vuitton','posizione'=>'Galleria Vittorio Emanuele II, Milano MI','descrizione'=>'Louis Vuitton è un rinomato marchio di moda francese famoso per i suoi lussuosi prodotti di pelletteria e accessori.'
-                ,'tipologia'=>'ONLINE','logo'=>'??'],
+                ,'tipologia'=>'ONLINE','logo'=>'img/lv_logo.jpeg'],
             ['partitaIva'=>'15473823894','nome'=>'Yves Saint Laurent','posizione'=>'Via de Tornabuoni, 48, Firenze FI','descrizione'=>'Yves Saint Laurent è una rinomata casa di moda e profumi di lusso, conosciuta per il suo stile elegante e sofisticato.'
-                ,'tipologia'=>'ONLINE','logo'=>''],
-//            ['partitaIva'=>'09876543219','nome'=>'Adidas','posizione'=>'Località Santa Filomena nn, Chieti CH','descrizione'=>'Adidas è un rinomato marchio di abbigliamento sportivo di origine tedesca, noto per la sua vasta gamma di prodotti che comprendono scarpe, abbigliamento e accessori.'
-//                ,'tipologia'=>'ONLINE','logo'=>''],
-//            ['partitaIva'=>'56373829744','nome'=>'Nike','posizione'=>'Località Santa Filomena nn, Chieti CH','descrizione'=>'Nike è un celebre marchio di abbigliamento sportivo statunitense, specializzato in scarpe, abbigliamento e attrezzature sportive.'
-//                ,'tipologia'=>'ONLINE','logo'=>''],
-//            ['partitaIva'=>'45276849302','nome'=>'Gucci','posizione'=>'Via dei Condotti, 8, Roma RM','descrizione'=>'Gucci: Lusso italiano e stile sofisticato in un unica riga.'
-//                ,'tipologia'=>'ONLINE','logo'=>''],
-//            ['partitaIva'=>'12345678901','nome'=>'Amarcord','posizione'=>'Piazza della Repubblica, 1c, Ancona AN','descrizione'=>'La novità ad Ancona si chiama Amarcord: un bar ristorante che, dalla mattina alla sera, ti accoglierà con tante gustose specialità e la cortesia di uno staff professionale'
-//                ,'tipologia'=>'ONLINE'],
-//            ['partitaIva'=>'34526627980','nome'=>'Med Store','posizione'=>'Corso Stamira, 45, Ancona AN','descrizione'=>'MedStore si rinnova, dopo 40 anni di esperienza nel mondo dell’elettronica di consumo, 15 anni nel mondo del retail e 10 nel mondo dell’eCommerce è giunto il tempo di intraprendere una nuova strada.'
-//                ,'tipologia'=>'ONLINE','logo'=>''],
+                ,'tipologia'=>'ONLINE','logo'=>'img/pngwing.com.png'],
+            ['partitaIva'=>'09876543219','nome'=>'Adidas','posizione'=>'Località Santa Filomena nn, Chieti CH','descrizione'=>'Adidas è un rinomato marchio di abbigliamento sportivo di origine tedesca, noto per la sua vasta gamma di prodotti che comprendono scarpe, abbigliamento e accessori.'
+                ,'tipologia'=>'ONLINE','logo'=>'img/Adidas_logo.png'],
+            ['partitaIva'=>'56373829744','nome'=>'Nike','posizione'=>'Località Santa Filomena nn, Chieti CH','descrizione'=>'Nike è un celebre marchio di abbigliamento sportivo statunitense, specializzato in scarpe, abbigliamento e attrezzature sportive.'
+                ,'tipologia'=>'ONLINE','logo'=>'img/Nike_logo.png'],
+            ['partitaIva'=>'45276849302','nome'=>'Coop','posizione'=>'Via Carlo Maratta, 30, Ancona AN','descrizione'=>'Coop: Una catena di supermercati e ipermercati molto diffusa, offrendo una vasta gamma di prodotti alimentari, prodotti per la casa, articoli per la cura personale e altro ancora.'
+                ,'tipologia'=>'ONLINE','logo'=>'img/Coop_logo.png'],
+            ['partitaIva'=>'12345678901','nome'=>'Mediaworld','posizione'=>'Via 1º Maggio, 25 Freestand, Ancona AN','descrizione'=>'Mediaworld: Un rinomato rivenditore di elettronica e prodotti tecnologici, offrendo una vasta selezione di dispositivi, elettrodomestici, prodotti audio e video, computer, telefoni cellulari e accessori.'
+                ,'tipologia'=>'ONLINE', 'logo'=>'img/Mediaworld_logo.png'],
+            ['partitaIva'=>'34526627980','nome'=>'Med Store','posizione'=>'Corso Stamira, 45, Ancona AN','descrizione'=>'MedStore si rinnova, dopo 40 anni di esperienza nel mondo dell’elettronica di consumo, 15 anni nel mondo del retail e 10 nel mondo dell’eCommerce è giunto il tempo di intraprendere una nuova strada.'
+                ,'tipologia'=>'ONLINE','logo'=>'img/MedStore_logo.png'],
         ]);
     }
 }
+
