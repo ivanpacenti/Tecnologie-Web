baabb9de63e5d25e006e9d7c6e61ca8e9c03600e
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-05-30 11:51:18 +0200

    fix pagina

--- resources/views/userView/coupongiaComprato.blade.php
+++ resources/views/userView/coupongiaComprato.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/catalogo_pub_design.css') }}" >
     <div class="coupon-informations">
-        <h1> Si è verificato un errore... </h1>
+        <h1> OPS...</h1>
         <h3> {{ Auth::user()->name }}  {{ Auth::user()->surname }} ci dispiace informarla che ha già acquistato questa offerta</h3>
         <h3> Siamo sicuri ne troverà altre di suo gradimento, continui pure su catalogo</h3>
         <button onclick="location.href='{{ route('catalogo') }}'" class="form-button">Torna al catalogo</button>
