30f4c62cda8209aae8a37532c4c64dc1678d12d5
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-06-04 17:50:13 +0200

    fix commento

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,39 +5,6 @@
 
 @section('content')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/admin_desing.css') }}" >
-    {{--<div class="Wrapper">
-        <h1>
-            BENVENUTO NELLA SEZIONE RISERVATA DEL SITO
-        </h1>
-        <div class="menu">
-            <h3>Funzioni disponibili</h3>
-            <details>
-                <summary>Faq</summary>
-                <div>
-                    C.R.U.D delle faq
-                </div>
-            </details>
-            <details>
-                <summary>Statistiche</summary>
-                <div>
-                    Visualizzazione di diverse statiche, come visualizzazione numero coupon emessi
-                </div>
-            </details>
-            <details>
-                <summary>Aziende</summary>
-                <div>
-                    C.R.U.D. delle aziende
-                </div>
-            </details>
-            <details>
-                <summary>Staff</summary>
-                <div>
-                    Gestione e assegnazione compiti come membro senior
-                </div>
-
-            </details>
-        </div>
-    </div>--}}
 
     <div class="maincontainer">
         <h1>
