9dc5c79d700a231dbcbb43da9d36dc51239d43d0
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-06-04 17:43:55 +0200

    pagina profilo admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/admin_desing.css') }}" >
-    <div class="Wrapper">
+    {{--<div class="Wrapper">
         <h1>
             BENVENUTO NELLA SEZIONE RISERVATA DEL SITO
         </h1>
@@ -37,6 +37,37 @@
 
             </details>
         </div>
+    </div>--}}
+
+    <div class="maincontainer">
+        <h1>
+            BENVENUTO NELLA SEZIONE RISERVATA
+        </h1>
+
+        <details>
+            <summary>Faq</summary>
+            <div>
+                Gestione delle FAQs
+            </div>
+        </details>
+        <details>
+            <summary>Aziende</summary>
+            <div>
+                Gestione delle aziende
+            </div>
+        </details>
+        <details>
+            <summary>Staff</summary>
+            <div>
+                Gestione dello staff
+            </div>
+        </details>
+        <details>
+            <summary>Statistiche</summary>
+            <div>
+                Visualizzazione delle statistiche
+            </div>
+        </details>
     </div>
 @endsection('content')
 
