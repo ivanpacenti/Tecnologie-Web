32704289671dac3caefaf6b81876d35ff448148e
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-06-05 10:34:52 +0200

    fix generali

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,21 +5,21 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/footer_design.css') }}" >
     <br>
         <div class="footercontainer1">
-            <div class="section">
+            <div class="section-footer">
                 <a href="{{route("index")}}">CHI SIAMO</a>
             </div>
 
-            <div class="section">
+            <div class="section-footer">
                 <a href="{{route("dovesiamo")}}">DOVE SIAMO?</a>
             </div>
 
-            <div class="section">
+            <div class="section-footer">
                 CONTATTACI
                 <p>+39 335 89308426</p>
                 <p><a  href="mailto:coupon@gmail.com">coupon@gmail.com</a></p>
             </div>
 
-            <div class="section">
+            <div class="section-footer">
                 <a href="{{route("visualizza_listafaq")}}"> FAQ</a>
             </div>
         </div>
