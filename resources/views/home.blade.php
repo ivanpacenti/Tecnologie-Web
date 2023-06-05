63c0cf02716d302d1320b31df9c95dc31b7ead77
Nicola Picciafuoco <s1099360@studenti.univpm.it>
2023-06-05 18:53:58 +0200

    inserimento tesina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@
             <p> Siamo lieti di annunciare che abbiamo selezionato alcune aziende di eccellenza per la nostra collaborazione. Abbiamo dedicato tempo ed energie per identificare partner di alta qualità, che rispecchiano i nostri valori e obiettivi.
                 Siamo fiduciosi che queste aziende saranno in grado di offrire prodotti e servizi di livello superiore.
                 Per scoprire quali aziende abbiamo selezionato, ti invitiamo a fare clic <a href="{{asset('homeAziende')}}">qui</a>.</p>
-            <p>Clicca <a href="documento.pdf" target="_blank">qui</a> per accedere alla documentazione.</p>
+            <p>Clicca <a href="Tecnologie_Web_tesina.pdf" target="_blank">qui</a> per accedere alla documentazione.</p>
         </div>
         <script src="js/scriptHome.js" async></script>
     </div>
